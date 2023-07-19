3fdcc2fd97ea0d92e848dab7207be212d86967ff
Theo Powers <theo@usemerkle.com>
2023-07-19 14:54:32 -0400

    fix: settler contract

--- src/MerkleOrderTaker.sol
+++ src/MerkleOrderTaker.sol
@@ -4,17 +4,32 @@ pragma solidity ^0.8.13;
 import "../src/MerkleOrderSettler.sol";
 
 contract Taker is MerkleOrderTaker {
+    // perform a trade
     function take(Order memory _order, uint256 minPayment, bytes calldata data) external returns (bool) {
-        (uint256 amountOut, ERC20 tokenOut) = (_order.amountOut, ERC20(_order.tokenOut));
-        // Transfer maker tokens to settler
-        tokenOut.transfer(msg.sender, amountOut);
+        ERC20 tokenIn = ERC20(_order.tokenIn);
+        ERC20 tokenOut = ERC20(_order.tokenOut);
 
-        // Transfer minEthPayment to msg.sender (settler)
+        bool isMaximizingOutput = _order.maximizeOut;
+        uint256 inputAmount = tokenIn.balanceOf(this); // calculate this somehow
+
+        // .... DO SWAPPING ....
+
+        uint256 outputAmount = 0; // calculate this somehow
+
+        // transfer the output token to the settler contract
+        tokenOut.transfer(msg.sender, outputAmount);
+
+        // if not maximize out, return the ununsed input token to the settler contract
+        if (!isMaximizingOutput) {
+            tokenIn.transfer(msg.sender, inputAmount);
+        }
+
+        // transfer eth payment to settler contract
         address payable caller = payable(msg.sender);
+
         caller.transfer(minPayment);
 
-        // do more things: backruns, liquidations, etc.
-        //  (...) = abi.decode(data, (...));
+        // make sure to return trues
         return true;
     }
 }
